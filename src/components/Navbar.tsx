c3e14a66e78ac96cb938e1588c184796d395b84d
Diwash <diwashdahal75@gmail.com>
2023-06-05 22:02:43 +0545

    feat: make navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const NAV_LINKS = [
   "Features",
@@ -10,14 +12,25 @@ const NAV_LINKS = [
 ];
 
 function Navbar() {
+  const [navbarActive, setNavbarActive] = useState(false);
+
   return (
-    <div className="flex fixed justify-between items-center px-36 w-screen h-[100px] bg-white">
+    <div className="flex fixed justify-between items-center px-5 md:px-36 w-screen h-[100px] bg-white">
       <Image src="/images/icon.png" alt="logo" height={40} width={40} />
-      <div className="flex gap-10 font-medium">
+      <div className="hidden gap-10 font-medium md:flex">
         {NAV_LINKS.map((item, index) => {
           return <Link href="#">{item}</Link>;
         })}
       </div>
+      <div
+        className="flex gap-10 font-medium md:hidden"
+        onClick={() => setNavbarActive((prev) => !prev)}
+      >
+        <AiOutlineMenu size={30} />
+      </div>
+      {navbarActive && (
+        <div className="absolute top-[100px] w-screen h-screen">kaalo</div>
+      )}
     </div>
   );
 }

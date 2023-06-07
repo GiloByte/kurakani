3a20339fba32d4b93ec01858b91b195daa204299
Diwash <diwashdahal75@gmail.com>
2023-06-07 22:08:21 +0545

    feat: add chat footer

--- src/components/Chat/ChatBody.tsx
+++ src/components/Chat/ChatBody.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 function ChatBody() {
-  return <div className="basis-[85%]">ChatBody</div>;
+  return (
+    <div className="basis-[85%]">
+    </div>
+  );
 }
 
 export default ChatBody;
--- src/components/Chat/ChatFooter.tsx
+++ src/components/Chat/ChatFooter.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiFillPlusCircle, AiFillLike } from "react-icons/ai";
+import { BsImage, BsEmojiSmileFill, BsSendFill } from "react-icons/bs";
+import { IoMdSend } from "react-icons/io";
 
 function ChatFooter() {
-  return <div className="basis-[8%] border-t-2">footer</div>;
+  const [message, setMessage] = useState<string>("");
+
+  const handleChange = (e: any) => {
+    setMessage(e.target.value);
+  };
+
+  const sendMessage = () => {
+    console.log("hi");
+  };
+  return (
+    <div className="basis-[8%] border-t-2 p-2 flex items-center gap-4">
+      {message.length === 0 && (
+        <>
+          <AiFillPlusCircle size={20} className="cursor-pointer text-primary" />
+          <BsImage size={20} className="cursor-pointer text-primary" />
+        </>
+      )}
+      <div className="relative w-full">
+        <BsEmojiSmileFill
+          size={20}
+          className="cursor-pointer absolute top-[6px] right-2 text-primary"
+        />
+        <form onSubmit={sendMessage}>
+          <input
+            type="text"
+            className="p-2 w-full h-8 bg-gray-100 rounded-full transition-all focus:outline-none"
+            placeholder="Aa"
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+      {message.length === 0 ? (
+        <AiFillLike size={28} className="cursor-pointer text-primary" />
+      ) : (
+        <IoMdSend
+          size={28}
+          className="cursor-pointer text-primary"
+          onClick={sendMessage}
+        />
+      )}
+    </div>
+  );
 }
 
 export default ChatFooter;

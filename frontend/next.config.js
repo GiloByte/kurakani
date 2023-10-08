c379ccfcc522842b15535be49bbb28a995bacfe5
Diwash Dahal <diwashdahal75@gmail.com>
2023-10-08 16:04:24 +0545

    fix: prevent webpack configs warnings

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
 };
 
-module.exports = nextConfig;
+module.exports = {
+  ...nextConfig,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals.push({
+        bufferutil: "bufferutil",
+        "utf-8-validate": "utf-8-validate",
+        "supports-color": "supports-color",
+      });
+    }
+
+    return config;
+  },
+};

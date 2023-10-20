98bde6c4faad87197384177579e672f6a6333c83
abishek0057 <abishekparajuli64@gmail.com>
2023-10-20 13:35:35 +0545

    feat: add send image feature

--- frontend/src/components/Chat/ChatFooter.tsx
+++ frontend/src/components/Chat/ChatFooter.tsx
@@ -3,8 +3,9 @@ import { useUser } from "@/contexts/UserContext";
 import React, { useRef, useState } from "react";
 import { AiFillPlusCircle, AiFillLike } from "react-icons/ai";
 import { BsImage, BsEmojiSmileFill } from "react-icons/bs";
-import { IoMdSend } from "react-icons/io";
+import { IoMdSend, IoMdCloseCircle } from "react-icons/io";
 import Picker from "emoji-picker-react";
+import Toast from "../shared/Toast";
 
 function ChatFooter({ roomId }: { roomId: string }) {
   const [message, setMessage] = useState<string>("");
@@ -12,6 +13,10 @@ function ChatFooter({ roomId }: { roomId: string }) {
   const { username } = useUser();
   const [showEmojiPicker, setShowEmojiPicker] = useState<boolean>(false);
   const inputRef = useRef<any | null>(null);
+  const fileRef = useRef<any | null>(null);
+  const [image, setImage] = useState<string | null>(null);
+  const [showToast, setShowToast] = useState<boolean>(false)
+  const [toastMessage, setToastMessage] = useState<string>("")
 
   const onEmojiPick = (emojiObj: any) => {
     setMessage((prevInput) => prevInput + emojiObj.emoji);
@@ -21,31 +26,80 @@ function ChatFooter({ roomId }: { roomId: string }) {
 
   const handleSendMessage = (e: any, message: string) => {
     e.preventDefault();
-    if (message.trim()) {
+    if (message.trim() || image) {
       socket?.emit("send_message", {
         text: message,
         name: username,
         time: new Date(),
         socketId: socket.id,
         roomId: roomId,
+        image,
       });
     }
     setMessage("");
+    setImage(null);
   };
 
   const handleTyping = () => {
     socket?.emit("typing", message ? username + " is typing ..." : "");
   };
 
+  const handleImageErrors = (e: any) => {
+    const data = e.target.files[0];
+    if (data.size > 1e7 || data.type.split("/")[0] !== "image"){
+      data.size > 1e7 ? setToastMessage("Image size should not exceed 10 MB") : setToastMessage("Please use a valid image format")
+      e.target.value = "";
+      setShowToast(true);
+      setTimeout(() => setShowToast(false), 1500)
+      return true
+    };
+    return false
+  };
+
+  const handleImageUpload = (e: any) => {
+    setImage(null);
+    const data = e.target.files[0];
+    if(handleImageErrors(e)) return
+    const reader = new FileReader();
+    reader.onloadend = function () {
+      const base64 = reader.result as string
+      setImage(base64);
+      e.target.value = "";
+    };
+    reader.readAsDataURL(data);
+  };
+
   return (
+    <>
+    {
+      showToast && <Toast message={toastMessage}/>
+    }
+    { image &&
+      <div className="relative border border-primary rounded-lg max-w-[6rem] h-24 ml-4 mb-1">
+        <IoMdCloseCircle size={20} className="absolute -right-2 -top-2 text-xs cursor-pointer text-red-600" onClick={() => setImage(null)}/>
+        <img src={image} className="w-full h-full object-contain"/>
+      </div>
+    }
     <div className="basis-[8%] border-t-2 p-2 flex items-center gap-4">
       {message.length === 0 && (
         <>
           <AiFillPlusCircle size={20} className="cursor-pointer text-primary" />
-          <BsImage size={20} className="cursor-pointer text-primary" />
+          <BsImage
+            size={20}
+            className="cursor-pointer text-primary"
+            onClick={() => fileRef.current.click()}
+          />
+          <input
+            type="file"
+            name="image" 
+            ref={fileRef}
+            accept="image/*"
+            onChange={handleImageUpload}
+            className="hidden"
+          />
         </>
       )}
-      <div className="relative w-full ">
+      <div className="relative w-full">
         <div className="absolute -right-8 sm:right-0 bottom-12 ">
           {showEmojiPicker && (
             <Picker
@@ -74,7 +128,7 @@ function ChatFooter({ roomId }: { roomId: string }) {
           />
         </form>
       </div>
-      {message.length === 0 ? (
+      {message.length === 0 && !image ? (
         <AiFillLike
           size={28}
           className="cursor-pointer text-primary"
@@ -88,6 +142,7 @@ function ChatFooter({ roomId }: { roomId: string }) {
         />
       )}
     </div>
+    </>
   );
 }
 
